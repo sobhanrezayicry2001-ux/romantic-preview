cacec67126981ca9e435f5ec07e53557c12e5a83
sobhanrezayicry2001-ux <sobhanrezayicry2001@gmail.com>
2026-05-06 01:51:09 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,31 +3,34 @@ const screenshot = document.getElementById('screenshot');
 const music = document.getElementById('music');
 const animatedText = document.getElementById('animatedText');
 
-const textToShow = `ملیکا، هنر یعنی یه فضا رو انقدر قشنگ بسازی
-که آدم روحش اونجا زندانی بشه.
-من زندانی شماره 0668 هستم.
-می خوام به یه دیت دعوتت کنم
-تا بیشتر در مورد حبس طولانی‌تر در این فضا صحبت کنیم.`;
+const textToShow = `این لحظه برای توست…
+لحظه‌ای پر از آرامش و عشق
+که با هم تجربه می‌کنیم.`;
 
 startBtn.addEventListener('click', () => {
+
   startBtn.style.display = 'none';
-  
+
+  // نمایش تصویر
   screenshot.style.display = 'block';
   setTimeout(() => {
     screenshot.style.opacity = 1;
   }, 50);
-  
-  music.play();
 
+  // پخش موزیک
+  music.play().catch(() => {});
+
+  // تایپ متن
   let index = 0;
-  const duration = 20000; // 20 ثانیه
-  const interval = duration / textToShow.length;
+  const speed = 60;
 
-  const typing = setInterval(() => {
-    animatedText.textContent += textToShow[index];
-    index++;
-    if(index >= textToShow.length){
-      clearInterval(typing);
+  function typeText() {
+    if (index < textToShow.length) {
+      animatedText.textContent += textToShow[index];
+      index++;
+      setTimeout(typeText, speed);
     }
-  }, interval);
+  }
+
+  typeText();
 });
